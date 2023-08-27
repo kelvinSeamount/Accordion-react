374c0d8c0a76afc16c13dd9edd3fdab1379e2fc3
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-08-27 23:11:57 +0200

    className change

--- src/AccordionItem.js
+++ src/AccordionItem.js
@@ -10,7 +10,7 @@ export default function AccordionItem({ faqsobj, num }) {
     <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <div className="flex">
         <p>{num + 1}</p>
-        <p className="text">{faqsobj.title}</p>
+        <p className="title">{faqsobj.title}</p>
         <p className="icon">{isOpen ? "-" : "+"}</p>
       </div>
       {isOpen && <div className="content-box">{faqsobj.text}</div>}
